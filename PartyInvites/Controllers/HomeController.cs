7f9b1a6c07f141dc2b3ac7509f69ca98f6d0cd97
Frank J. Reashore <reashore@telus.net>
2018-10-24 12:41:35 -0700

    Fixed views

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -10,19 +10,24 @@ namespace PartyInvites.Controllers
 
         public HomeController(PartyInvitesDbContext partyInvitesDbContext) => _partyInvitesDbContext = partyInvitesDbContext;
 
+        [HttpGet]
         public IActionResult Index() => View();
 
+        [HttpGet]
         public IActionResult Respond() => View();
 
+        [HttpPost]
         public IActionResult Respond(GuestResponse guestResponse)
         {
             _partyInvitesDbContext.GuestResponses.Add(guestResponse);
             _partyInvitesDbContext.SaveChanges();
-            return RedirectToAction("Thanks", new {guestResponse.Name, guestResponse.WillAttend});
+            return RedirectToAction(nameof(Thanks), new {guestResponse.Name, guestResponse.WillAttend});
         }
 
+        [HttpGet]
         public IActionResult Thanks(GuestResponse guestResponse) => View(guestResponse);
 
+        [HttpGet]
         public IActionResult ListResponses() => View(_partyInvitesDbContext.GuestResponses.OrderByDescending(r => r.WillAttend));
     }
 }
